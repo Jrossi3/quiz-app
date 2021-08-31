972bae506f9a852d606176e408591281276384b1
Jrossi3 <55547488+Jrossi3@users.noreply.github.com>
2021-08-31 13:38:38 -0400

    Buttons + js
    Fixed some button features, added to js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var quizContainer = document.getElementById('quiz');
+var quizContainer = document.getElementById('quizQuestions');
+var answerChoiceA = document.getElementById('a');
+var answerChoiceB = document.getElementById('b');
+var answerChoiceC = document.getElementById('c');
+var answerChoiceD = document.getElementById('d');
+var questionCounter = 0;
 
 var quizData = [
     {
@@ -22,7 +27,7 @@ var quizData = [
         correctAnswer: 'a'
     },
     {
-        question: "What is my name?", 
+        question: "What is my", 
         answerChoices: {
             a: 'Jason', 
             b: 'Dino', 
@@ -53,21 +58,35 @@ var quizData = [
     }
 ];
 
-let quizQuestions = 0;
-
 function quizCall() {
     //Starts the quiz and keeps track of the quizData array
-    var quizDataQuestions = quizData[quizQuestions];
-    quizContainer.innerHTML = quizDataQuestions;
-
+    var quizDataQuestions;
+    quizDataQuestions = quizData[questionCounter];
+    quizContainer.innerHTML = quizData.question;
+    console.log(quizData[questionCounter], questionCounter);
 }
 
 function nextQuestion() {
     //goes to the next question
+    questionCounter++;
+    if (questionCounter < quizData.length) {
+        quizDataQuestions = quizData[questionCounter];
+        quizContainer.innerHTML = quizData[questionCounter];
+        quizContainer.innerHTML = quizData[questionCounter];
+        quizContainer.innerHTML = quizData[questionCounter];
+        quizContainer.innerHTML = quizData[questionCounter];
+        console.log(quizData[questionCounter], questionCounter);
+    }
 }
 
 function previousQuestion() {
     //goes back to the previous question
+    counter--;
+    if (questionCounter < quizData.length) {
+        quizDataQuestions = quizData[counter];
+        quizContainer.innerHTML = quizData.question;
+        console.log(quizData[counter], counter);
+    }
 }
 
 function submit() {
